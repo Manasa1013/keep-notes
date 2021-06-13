405e5073442031d09c39716f93ca29a6478740ab
Manasa1013 <66269691+Manasa1013@users.noreply.github.com>
2021-06-13 22:22:47 +0530

    add displayCard component

---
+++ my-app/src/components/displaycard/display-card.jsx
@@ -0,0 +1,38 @@
+import { EditCard } from "../editcard/edit-card";
+import { PinnedCards } from "./pinned-cards";
+import { OtherCards } from "./other-cards";
+import { useTask } from "../../contexts/task-context";
+import "./displaycard.css";
+export function DisplayCard({displayRef}){
+    const { state , dispatch } = useTask();
+    let pinnedList = state.taskList.filter(taskItem => taskItem.isPinned);
+    console.log(pinnedList);
+    let othersList = state.taskList.filter(taskItem => !taskItem.isPinned);
+    console.log(othersList);
+    return (
+      <div className="wrapper">
+        <section className="card-container displaycontainer" 
+          ref={displayRef} >
+            <p>Pinned Cards</p>
+        {
+            state.taskList.filter(taskItem => taskItem.isPinned)
+                          .sort((a,b) => a.sortKey > b.sortKey ? -1 : 1)
+                          .map((taskItem) => {
+                              console.log(taskItem);
+                              return <PinnedCards taskItem={taskItem} />
+                         })
+        }
+            <p>Other Cards</p>
+        {
+          state.taskList.filter(taskItem => !taskItem.isPinned)
+                        .sort((a,b) => a.sortKey > b.sortKey ? -1 : 1)
+                        .map(taskItem => {
+                          return <OtherCards taskItem={taskItem} />
+                        })
+        }
+      </section>
+    </div>
+    )
+}
+
+ 
